63b5c6c9df74ff7079c8c50aac7b7b7a1bf7f031
Artem Suprun <oxymoron.dr@gmail.com>
2023-05-05 20:07:40 +0200

    feat: add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const pathToFolder = path.join(__dirname, "files");
+const pathToCopyFolder = path.join(__dirname, "files-copy");
 
+fs.mkdir(pathToCopyFolder, { recursive: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Directory created successfully!");
+});
+
+const infoFiles = (file) => {
+  if (file.isFile()) {
+    const pathItem = path.join(pathToFolder, file.name);
+    const pathToCopyItem = path.join(pathToCopyFolder, file.name);
+
+    fs.copyFile(pathItem, pathToCopyItem, (err) => {
+      if (err) throw err;
+      console.log(`${file.name} was copied`);
+    });
+  }
+};
+
+fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  files.forEach((item) => {
+    infoFiles(item);
+  });
+});
